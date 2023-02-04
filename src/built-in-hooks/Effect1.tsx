46a6ccbf24a300e1e0e6f019aa7dde08db2e6790
devlin <webmasterdevlin@gmail.com>
2023-02-04 11:44:55 +0100

    update

--- src/built-in-hooks/Effect1.tsx
+++ src/built-in-hooks/Effect1.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 
+// The useEffect Hook allows you to perform side effects in your components.
+// Some examples of side effects are: fetching data, directly updating the DOM, and timers.
+// useEffect accepts two arguments.
+// The second argument is optional. useEffect(<function>, <dependency>)
 export default function Effect1() {
   useEffect(() => {}, []);
 
