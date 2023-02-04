6c3548f8789ab9426e61800db3955cf73944ed8c
devlin <webmasterdevlin@gmail.com>
2023-02-04 11:44:10 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
+import Effect1 from './built-in-hooks/Effect1';
 import State from './built-in-hooks/State';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="container mx-auto px-4">
-        <State />
+        {/* <State /> */}
+        <Effect1 />
       </div>
     </BrowserRouter>
   );
